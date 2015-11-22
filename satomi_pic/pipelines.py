f6ad6afbae3536b66b797f042e20d16be9baa702
sjtu-lyj <sjtu5140809011@gmail.com>
2015-11-22 11:54:13 +0800

    your commit

---
+++ satomi_pic/pipelines.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+from scrapy import Request
+
+class SatomiPicPipeline(object):
+    def process_item(self, item, spider):
+        # t=item['content'].decode('utf-8')
+        t=item['content'].decode('utf-8')
+        path='/home/church-father/Documents/novel/new3.txt'
+        output=open(path,'a')
+        output.write(t)
+        output.close()
+        return item
+    def item_completed(self,results,item,info):
+    	path='/home/church-father/Documents/novel/new3.txt'
+        output=open(path,'a')
+        output.write(item['content'])
+        output.close()
+        return item
---
+++ satomi_pic/spiders/satomi_spider.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+__author__ = 'church-father'
+
+from scrapy.spiders import Rule,CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from satomi_pic.items import SatomiPicItem
+
+
+class SatomiSpider(CrawlSpider):
+    name="satomi_pic_spider"
+
+    #download_delay=1
+
+    start_urls=[
+        "http://www.kanshu.la/book/zetianji/8357143.shtml"
+    ]
+
+    rules=(
+        # Rule(LinkExtractor(allow=(r'\d{7}.html'),restrict_xpaths=('//a[starts-with(@id=a_03)]')),callback='parse_item',follow=True),
+        Rule(LinkExtractor(allow=(r'\d{7}.html')),callback='parse_item',follow=True),
+        # Rule(LinkExtractor(allow=(r'\d{7}.html'),restrict_xpaths=('//a[@id=a_03]')),callback='parse_item',follow=True),
+    )
+
+    def parse_item(self, response):
+        sel=Selector(response)
+        item=SatomiPicItem()
+
+        if response.url>="http://www.kanshu.la/book/zetianji/8357143.shtml":
+
+            title=sel.xpath('//title/text()').extract()
+            item['title']=[t.encode('utf-8') for t in title]
+            path='/home/church-father/Documents/zetianji2/title'
+            output2=open(path,'a')
+            ss=''
+            n=0
+            while n<len(item['title']):
+                ss+=str(item['title'][n])
+                n+=1
+            output2.write(ss)
+            output2.write('\n')
+
+            content=sel.xpath('//div[@id="contentTxt"]/text()').extract()
+            item['content']=[t.encode('utf-8') for t in content]
+            path='/home/church-father/Documents/zetianji2/'+response.url
+            output1=open(path,'a')
+            ss1=''
+            n=0
+            while n<len(item['content']):
+                ss1+=str(item['content'][n])
+                n+=1
+            output1.write(ss)
+            output1.write(ss1)
+            output1.close()
+
+            yield item
